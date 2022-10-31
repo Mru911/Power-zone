fc5c5e9647ca3f0eefb6e259e07f0f0d75961a8e
Mrugank <mrugankkiran@gmail.com>
2022-10-31 23:27:42 +0530

    ok

---
+++ src/utils/fetchData.js
@@ -0,0 +1,23 @@
+export const exerciseOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'f11f1e334dmsh091250a3734af51p1de40bjsn45e18df2889f',
+        'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+    },
+  };
+  export const youtubeOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'f11f1e334dmsh091250a3734af51p1de40bjsn45e18df2889f',
+        'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
+    },
+  };
+  
+ 
+  
+  export const fetchData = async (url, options) => {
+    const res = await fetch(url, options);
+    const data = await res.json();
+  
+    return data;
+  };
